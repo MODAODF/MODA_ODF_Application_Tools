f1665fbd14f86438d83b5609c2aa40b9198d9f9a
AlanTom <alan.tang@ossii.com.tw>
2020-03-09 16:06:17 +0800

    o[version]: remove product version for msi title & windows app title
    Change-Id: I11772393ea099443532bd9081eb043c539639bce

--- solenv/bin/modules/installer/ziplist.pm
+++ solenv/bin/modules/installer/ziplist.pm
@@ -718,6 +718,7 @@ sub set_default_productversion_if_required
     {
         $allvariables->{'PRODUCTVERSION'} = 1;  # FAKE
     }
+    $allvariables->{'PRODUCTVERSION'} = " ";
 }
 
 ####################################################

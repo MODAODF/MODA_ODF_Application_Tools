606a6b60e015c50adedef181e1f11a8c32095e69
AlanTom <alan.tang@ossii.com.tw>
2020-08-17 15:15:30 +0800

    Revert "o[print][m1316]: adjusted leftmargin for print"
    This reverts commit 40f6d770892327f257ec9874532440efc9c2e26f.

--- editeng/source/items/frmitems.cxx
+++ editeng/source/items/frmitems.cxx
@@ -529,8 +529,6 @@ void SvxLRSpaceItem::AdjustLeft()
         nLeftMargin = nTxtLeft + nFirstLineOfst;
     else
         nLeftMargin = nTxtLeft;
-    // mts_1316 adjusted leftmargin for print
-    nLeftMargin = nLeftMargin * 1.5;
 }
 
 

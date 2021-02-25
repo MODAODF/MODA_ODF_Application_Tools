9911b445ae66116962fd133cf935a3846143d098
Caolán McNamara <caolanm@redhat.com>
2021-02-25 17:03:31 +0000

    drop intermediate vcl container for these welded floating toplevels
    Change-Id: I4a528485de62a0e0acabd41abf6872e2f0e1710e
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/111567
    Tested-by: Caolán McNamara <caolanm@redhat.com>
    Reviewed-by: Caolán McNamara <caolanm@redhat.com>
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/114409
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    Reviewed-by: Szymon Kłos <szymon.klos@collabora.com>

--- vcl/inc/salvtables.hxx
+++ vcl/inc/salvtables.hxx
@@ -120,6 +120,8 @@ public:
 
     virtual std::unique_ptr<weld::Menu> weld_menu(const OString& id) override;
 
+    virtual std::unique_ptr<weld::Popover> weld_popover(const OString& id) override;
+
     virtual std::unique_ptr<weld::Toolbar> weld_toolbar(const OString& id) override;
 
     virtual std::unique_ptr<weld::SizeGroup> create_size_group() override;

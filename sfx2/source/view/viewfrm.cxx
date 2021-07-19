3ecb98954a35c6d589f266ac94a9d1b498bc969b
AlanTom <alan.tang@ossii.com.tw>
2021-07-19 17:27:13 +0800

    o[views]: remove involvement and donations bar
    Change-Id: Id4ed960429b0ea292748c2ebb32fb7334a87f6ed

--- sfx2/source/view/viewfrm.cxx
+++ sfx2/source/view/viewfrm.cxx
@@ -1357,7 +1357,7 @@ void SfxViewFrame::Notify( SfxBroadcaster& /*rBC*/, const SfxHint& rHint )
                 } //bShowTipOfTheDay
 
                 // inform about the community involvement
-                const sal_Int64 nLastGetInvolvedShown = officecfg::Setup::Product::LastTimeGetInvolvedShown::get();
+                /*const sal_Int64 nLastGetInvolvedShown = officecfg::Setup::Product::LastTimeGetInvolvedShown::get();
                 const sal_Int64 nNow = std::chrono::duration_cast<std::chrono::seconds>(t0).count();
                 const sal_Int64 nPeriodSec(60 * 60 * 24 * 180); // 180 days in seconds
                 bool bUpdateLastTimeGetInvolvedShown = false;
@@ -1384,10 +1384,10 @@ void SfxViewFrame::Notify( SfxBroadcaster& /*rBC*/, const SfxHint& rHint )
                     std::shared_ptr<comphelper::ConfigurationChanges> batch(comphelper::ConfigurationChanges::create());
                     officecfg::Setup::Product::LastTimeGetInvolvedShown::set(nNow, batch);
                     batch->commit();
-                }
+                }*/
 
                 // inform about donations
-                const sal_Int64 nLastDonateShown = officecfg::Setup::Product::LastTimeDonateShown::get();
+                /*const sal_Int64 nLastDonateShown = officecfg::Setup::Product::LastTimeDonateShown::get();
                 bool bUpdateLastTimeDonateShown = false;
 
                 if (nLastDonateShown == 0)
@@ -1411,7 +1411,7 @@ void SfxViewFrame::Notify( SfxBroadcaster& /*rBC*/, const SfxHint& rHint )
                     std::shared_ptr<comphelper::ConfigurationChanges> batch(comphelper::ConfigurationChanges::create());
                     officecfg::Setup::Product::LastTimeDonateShown::set(nNow, batch);
                     batch->commit();
-                }
+                }*/
 
                 // read-only infobar if necessary
                 const SfxViewShell *pVSh;

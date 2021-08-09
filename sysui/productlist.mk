c2cd2448224b36324fd6a7f51f67778666e9dfe8
AlanTom <alan.tang@ossii.com.tw>
2021-08-09 18:17:30 +0800

    o[deb]: add for ubuntu deb packages name
    Change-Id: I58f433df5b8011a66b3e82569c2f1c8b05acf4b8

--- sysui/productlist.mk
+++ sysui/productlist.mk
@@ -7,15 +7,17 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 #
 
-PRODUCTLIST := libreoffice oxoffice ndcodfsys libreofficedev
+PRODUCTLIST := libreoffice oxoffice ndcodfsys libreofficedev ndcodfapplicationtools
 PKGVERSION := $(USERDIRPRODUCTVERSION).$(OXO_VERSION_MINOR)
 PKGVERSIONSHORT := $(USERDIRPRODUCTVERSION)
 PRODUCTNAME.libreoffice := LibreOffice
 PRODUCTNAME.oxoffice := OxOffice
+PRODUCTNAME.ndcodfapplicationtools := NDCODFApplicationTools
 PRODUCTNAME.ndcodfsys := NDCODFSYS
 PRODUCTNAME.libreofficedev := LibreOfficeDev
 UNIXFILENAME.libreoffice := libreoffice$(PKGVERSIONSHORT)
 UNIXFILENAME.oxoffice := oxoffice
+UNIXFILENAME.ndcodfapplicationtools := ndcodfapplicationtools
 UNIXFILENAME.ndcodfsys := ndcodfsys
 UNIXFILENAME.libreofficedev := libreofficedev$(PKGVERSIONSHORT)
 

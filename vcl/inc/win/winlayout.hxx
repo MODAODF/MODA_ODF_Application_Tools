1a9bdbadbdc15eda04369d2c7924df8bb2c2b2e3
Mark Hung <marklh9@gmail.com>
2021-11-01 20:53:19 +0800

    tdf#145322, tdf#144378 fix printing for vertical writing
    Printing still uses ExTextOutRenderer to render text because
    Skia haven't yet support printing and DWriteTextRenderer can't
    bind the printer DC.
    
    ExTextOutRenderer uses win32 API ExtTextOutW. In order to
    renderer upright CJK text in vertical writing with that API,
    the HFONT created with CreateFontIndirectW needs a font name
    prefixed with '@'. OTOH, use '@' prefixed font with Skia break
    the vertical writing unit test.
    
    - WinSalGraphics::ImplDoSetFont: use '@' prefixed font name
    if the requested font is vertical and is for printing.
    
    - ExTextOutRenderer: use SetTextAlign and text metric tmDescent
    to adjust vertical glyphs. It's not consistent with Skia or
    DWriteTextRenderer, and is still incorrect in many cases.
    
    The patch is adapted from reverting
    
    commit 5686c1aca40beb9514d40c86b4a3780a8a1334ba
    Author: Mark Hung <marklh9@gmail.com>
    Date:   Sun May 2 14:45:45 2021 +0800
    
        vcl: use DWriteTextRenderer for vertical writing.
    
    Change-Id: Ib2d3df8b68cad4bebe0672c9da0a16b62aed99e6
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/125978
    Tested-by: Jenkins
    Reviewed-by: Mark Hung <marklh9@gmail.com>
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/126545
    Reviewed-by: Adolfo Jayme Barrientos <fitojb@ubuntu.com>

--- vcl/inc/win/winlayout.hxx
+++ vcl/inc/win/winlayout.hxx
@@ -127,6 +127,10 @@ public:
 
     HFONT GetHFONT() const { return m_hFont; }
     float GetScale() const { return m_fScale; }
+    // Return true if the font is for vertical writing.
+    // I.e. the font name of the LOGFONT is prefixed with '@'.
+    bool  IsCJKVerticalFont() const { return m_bIsCJKVerticalFont; }
+    sal_Int32 GetTmDescent() const { return m_nTmDescent; }
 
     // Prevent deletion of the HFONT in the WinFontInstance destructor
     // Used for the ScopedFont handling
@@ -149,6 +153,8 @@ private:
     WinSalGraphics *m_pGraphics;
     HFONT m_hFont;
     float m_fScale;
+    bool  m_bIsCJKVerticalFont;
+    sal_Int32 m_nTmDescent;
     WinGlyphCache maWinGlyphCache;
 };
 

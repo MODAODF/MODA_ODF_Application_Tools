6c2ced950a67aa979e7a49c4199565bcb2993f90
Tomaž Vajngerl <tomaz.vajngerl@collabora.co.uk>
2021-12-07 23:25:54 +0100

    Add graphic size checker for the preferred document DPI
    This change adds a graphic size checker, which checks all the
    images in the document, if they largely differ (outside of 50% and
    110% of the image size) from the set preferred image DPI document
    setting. For all images that don't fall under this bounds, list
    them in the dialog and offer the posibility to select/goto the
    image and pop-up the properties dialog for the image to change
    its size.
    
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/127094
    Tested-by: Jenkins
    Reviewed-by: Tomaž Vajngerl <quikee@gmail.com>
    (cherry picked from commit 046e6cfa544d2ffd67fd29ba7dde41b495744618)
    
    Change-Id: I06efce77c291fdb6ec3864d72c2f4d15dba9c42b
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/127239
    Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
    Reviewed-by: Miklos Vajna <vmiklos@collabora.com>

--- sw/source/uibase/shells/basesh.cxx
+++ sw/source/uibase/shells/basesh.cxx
@@ -84,6 +84,7 @@
 #include <modcfg.hxx>
 #include <svx/fmshell.hxx>
 #include <SwRewriter.hxx>
+#include <GraphicSizeCheck.hxx>
 #include <svx/galleryitem.hxx>
 #include <com/sun/star/gallery/GalleryItemType.hpp>
 #include <memory>
@@ -1878,6 +1879,13 @@ void SwBaseShell::GetState( SfxItemSet &rSet )
                 else
                     rSet.Put( SfxVisibilityItem( nWhich, false ) );
                 break;
+            case SID_GRAPHIC_SIZE_CHECK:
+            {
+                sal_Int32 nDPI = rSh.GetDoc()->getIDocumentSettingAccess().getImagePreferredDPI();
+                if (nDPI <= 0)
+                    rSet.DisableItem(nWhich);
+            }
+            break;
         }
         nWhich = aIter.NextWhich();
     }
@@ -2697,6 +2705,13 @@ void SwBaseShell::ExecDlg(SfxRequest &rReq)
             sw::AccessibilityCheck aCheck(rSh.GetDoc());
             aCheck.check();
             svx::AccessibilityCheckDialog aDialog(pMDI, aCheck.getIssueCollection());
+            aDialog.run();
+        }
+        break;
+        case SID_GRAPHIC_SIZE_CHECK:
+        {
+            sw::GraphicSizeCheckGUIResult aResult(rSh.GetDoc());
+            svx::GenericCheckDialog aDialog(pMDI, aResult);
             aDialog.run();
         }
         break;

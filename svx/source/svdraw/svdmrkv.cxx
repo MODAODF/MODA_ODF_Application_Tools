LibreOffice/core

improve LOK notifyInvalidation
(*) tweak buffer in SfxLokHelper::notifyInvalidation to be a bit larger,
so we avoid the cost of a resize&copy
(*) use our optimised OString concatentation instead of going via
std::stringstream
(*) pass down a pointer to rectangle, instead of a string. later we will
use this to avoid doing the stringify until later

Change-Id: Ia3e3042bc919d9b9cb80e47a93704eb236438605

svx/source/svdraw/svdmrkv.cxx
@@ -251,9 +251,9 @@ void SdrMarkView::ModelHasChanged()
     {
         //TODO: Is MarkedObjRect valid at this point?
         tools::Rectangle aSelection(GetMarkedObjRect());
-        OString sSelection;
+        tools::Rectangle* pResultRect;
         if (aSelection.IsEmpty())
-            sSelection = "EMPTY";
+            pResultRect = nullptr;
         else
         {
             sal_uInt32 nTotalPaintWindows = this->PaintWindowCount();
@@ -282,11 +282,11 @@ void SdrMarkView::ModelHasChanged()
                 }
             }
 
-            sSelection = aSelection.toString();
+            pResultRect = &aSelection;
         }
 
         if(SfxViewShell* pViewShell = GetSfxViewShell())
-            SfxLokHelper::notifyInvalidation(pViewShell, sSelection);
+            SfxLokHelper::notifyInvalidation(pViewShell, pResultRect);
     }
 }
 

MODAODF/MODA_ODF_Application_Tools

Reviewed-on: https://gerrit.libreoffice.org/c/core/+/119994
Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
Reviewed-by: Michael Meeks <michael.meeks@collabora.com>
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/120072
Tested-by: Jenkins
Reviewed-by: Noel Grandin <noel.grandin@collabora.co.uk>
(cherry picked from commit 417f881d20cafe88a02b64894ba4483875fb9460)
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/122123

svx/source/svdraw/svdmrkv.cxx
@@ -230,9 +230,9 @@ void SdrMarkView::ModelHasChanged()
 
     //TODO: Is MarkedObjRect valid at this point?
     tools::Rectangle aSelection(GetMarkedObjRect());
-    OString sSelection;
+    tools::Rectangle* pResultSelection;
     if (aSelection.IsEmpty())
-        sSelection = "EMPTY";
+        pResultSelection = nullptr;
     else
     {
         sal_uInt32 nTotalPaintWindows = this->PaintWindowCount();
@@ -261,11 +261,11 @@ void SdrMarkView::ModelHasChanged()
             }
         }
 
-        sSelection = aSelection.toString();
+        pResultSelection = &aSelection;
     }
 
     if(SfxViewShell* pViewShell = GetSfxViewShell())
-        SfxLokHelper::notifyInvalidation(pViewShell, sSelection);
+        SfxLokHelper::notifyInvalidation(pViewShell, pResultSelection);
 }
 
 
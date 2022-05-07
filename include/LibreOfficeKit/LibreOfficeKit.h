fa61b30da04f3306fa575dad33a683c8c70f2fc4
Firefly <firefly@ossii.com.tw>
2022-05-07 11:11:37 +0800

    LOK: freemium: added new parameter in the API
    Added new parameter in the freemium API to make it consistent with other APIs
    
    Change-Id: I5e4e7ce24102209569a431e6e4fb2e92ebf77edc

--- include/LibreOfficeKit/LibreOfficeKit.h
+++ include/LibreOfficeKit/LibreOfficeKit.h
@@ -459,10 +459,13 @@ struct _LibreOfficeKitDocumentClass
                                 const char* pArguments);
 
     /// @see lok::Document::setFreemiumDenyList
-    void (*setFreemiumDenyList) (const char* freemiumDenyList);
+    void (*setFreemiumDenyList) (LibreOfficeKitDocument* pThis,
+                                const char* freemiumDenyList);
 
     /// @see lok::Document::setFreemiumView
-    void (*setFreemiumView) (int nViewId, bool isFreemium);
+    void (*setFreemiumView) (LibreOfficeKitDocument* pThis,
+                            int nViewId,
+                            bool isFreemium);
 
     /// Added by Firefly<firefly@ossii.com.tw>
     /// @see lok::Document::initUnoStatus

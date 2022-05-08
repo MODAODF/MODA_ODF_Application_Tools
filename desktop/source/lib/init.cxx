09852d59927669cd1ae566a285bfc25b34ba5c4a
Firefly <firefly@ossii.com.tw>
2022-05-08 14:00:04 +0800

    Send Calc Insert Headers and Footers state change to LOK
    Change-Id: Ibdaf553b7dc2c552cebd63dc0c669d3d723a08ba

--- desktop/source/lib/init.cxx
+++ desktop/source/lib/init.cxx
@@ -3093,6 +3093,7 @@ static void doc_iniUnoCommands ()
         OUString(".uno:NumberFormatPercent"),
         OUString(".uno:NumberFormatDecimal"),
         OUString(".uno:NumberFormatDate"),
+        OUString(".uno:EditHeaderAndFooter"),
         OUString(".uno:FrameLineColor"),
         OUString(".uno:SortAscending"),
         OUString(".uno:SortDescending"),

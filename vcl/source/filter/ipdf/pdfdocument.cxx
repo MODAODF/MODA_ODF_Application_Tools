53db4dea3206bfafbcef08d28c8dde0a575a6c6f
Jaume Pujantell <jaume.pujantell@collabora.com>
2023-03-03 19:25:11 +0100

    Fix a bug parsing pdf arrays
    The parser ignored number elements in some situations, like before
    a reference element. This manifested in creating an invalid pdf
    file when exporting as pdf a document that contains a pdf.
    
    Change-Id: I98625c8da8631056079814f7e824f36177cf41c7
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/148198
    Tested-by: Jenkins
    Reviewed-by: Andras Timar <andras.timar@collabora.com>
    (cherry picked from commit 574e89ccda1b389faca9f3e44d909a71b5599473)
    Reviewed-on: https://gerrit.libreoffice.org/c/core/+/148333
    Tested-by: Andras Timar <andras.timar@collabora.com>

--- vcl/source/filter/ipdf/pdfdocument.cxx
+++ vcl/source/filter/ipdf/pdfdocument.cxx
@@ -3202,6 +3202,18 @@ size_t PDFObjectParser::parse(PDFElement* pParsingElement, size_t nStartIndex, i
         }
         else if (auto pReference = dynamic_cast<PDFReferenceElement*>(pCurrentElement))
         {
+            // Handle previously stored number
+            if (aNumbers.size() > 2)
+            {
+                aNumbers.resize(aNumbers.size() - 2);
+                if (pParsingArray)
+                {
+                    for (auto& pNumber : aNumbers)
+                        pParsingArray->PushBack(pNumber);
+                }
+                aNumbers.clear();
+            }
+
             if (pParsingArray)
             {
                 pParsingArray->PushBack(pReference);
@@ -3222,6 +3234,17 @@ size_t PDFObjectParser::parse(PDFElement* pParsingElement, size_t nStartIndex, i
         }
         else if (auto pLiteralString = dynamic_cast<PDFLiteralStringElement*>(pCurrentElement))
         {
+            // Handle previously stored number
+            if (!aNumbers.empty())
+            {
+                if (pParsingArray)
+                {
+                    for (auto& pNumber : aNumbers)
+                        pParsingArray->PushBack(pNumber);
+                }
+                aNumbers.clear();
+            }
+
             if (pParsingArray)
             {
                 pParsingArray->PushBack(pLiteralString);
@@ -3239,6 +3262,17 @@ size_t PDFObjectParser::parse(PDFElement* pParsingElement, size_t nStartIndex, i
         }
         else if (auto pBoolean = dynamic_cast<PDFBooleanElement*>(pCurrentElement))
         {
+            // Handle previously stored number
+            if (!aNumbers.empty())
+            {
+                if (pParsingArray)
+                {
+                    for (auto& pNumber : aNumbers)
+                        pParsingArray->PushBack(pNumber);
+                }
+                aNumbers.clear();
+            }
+
             if (pParsingArray)
             {
                 pParsingArray->PushBack(pBoolean);
@@ -3256,6 +3290,17 @@ size_t PDFObjectParser::parse(PDFElement* pParsingElement, size_t nStartIndex, i
         }
         else if (auto pHexString = dynamic_cast<PDFHexStringElement*>(pCurrentElement))
         {
+            // Handle previously stored number
+            if (!aNumbers.empty())
+            {
+                if (pParsingArray)
+                {
+                    for (auto& pNumber : aNumbers)
+                        pParsingArray->PushBack(pNumber);
+                }
+                aNumbers.clear();
+            }
+
             if (pParsingArray)
             {
                 pParsingArray->PushBack(pHexString);

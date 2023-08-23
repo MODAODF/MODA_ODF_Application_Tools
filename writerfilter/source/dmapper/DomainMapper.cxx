LibreOffice/core

DOCX filter: improve handling of negative <w:position> in paragraph styles
The bugdoc has a <w:position w:val="-1"> in its Normal paragraph style,
which is almost not visible in Word, but we mapped this to default
subscript text in Writer, leading to very visible bad font height in
practice.

The root of the problem is that <w:position> works with an absolute
offset in half-points, while Writer works in percentages, so the
import/export code can only do a correct mapping in case the font size
is known. This initial mapping was added in commit
e70df84352d3670508a4666c97df44f82c1ce934 (try somewhat harder to read
w:position (bnc#773061), 2012-08-07), and later commit
d71cf6390a89ea6a4fab724e3a7996f28ca33661 (tdf#99602 writerfilter: import
subscript into character style, 2019-10-04) gave up on this for
character styles.

Fix the problem by working with paragraph styles similar to what the
binary DOC filter already does, just assuming that the font height from
the style won't be overwritten, or will be overwritten together with a
matching <w:position>. Do this only for negative <w:position> for now,
as that's good enough for our needs. Do the opposite of this at export
time.

It would be still possible in the future to add native handling for
absolute escapements, and then this mapping would not be needed at all.

Change-Id: I771c7bed27fa2596153aa77c472c91b819fa4cb1
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/152962
Reviewed-by: Miklos Vajna <vmiklos@collabora.com>
Tested-by: Jenkins

writerfilter/source/dmapper/DomainMapper.cxx
@@ -2061,6 +2061,13 @@ void DomainMapper::sprmWithProps( Sprm& rSprm, const PropertyMapPtr& rContext )
             {
                 // For some undocumented reason, MS Word seems to ignore this in docDefaults
 
+                const StyleSheetEntryPtr pCurrStyle = GetStyleSheetTable()->GetCurrentEntry();
+                if (pCurrStyle && pCurrStyle->m_nStyleTypeCode == STYLE_TYPE_PARA && nIntValue < 0)
+                {
+                    m_pImpl->deferCharacterProperty(nSprmId, uno::Any(nIntValue));
+                    break;
+                }
+
                 // DON'T FIXME: Truly calculating this for Character Styles will be tricky,
                 // because it depends on the final fontsize - regardless of
                 // where it is set. So at the style level,
@@ -3534,9 +3541,19 @@ void DomainMapper::sprmWithProps( Sprm& rSprm, const PropertyMapPtr& rContext )
     }
 }
 
-void DomainMapper::processDeferredCharacterProperties( const std::map< sal_Int32, uno::Any >& deferredCharacterProperties )
+void DomainMapper::ProcessDeferredStyleCharacterProperties()
 {
-    assert( m_pImpl->GetTopContextType() == CONTEXT_CHARACTER );
+    assert(m_pImpl->GetTopContextType() == CONTEXT_STYLESHEET);
+    m_pImpl->processDeferredCharacterProperties(false);
+}
+
+void DomainMapper::processDeferredCharacterProperties(
+    const std::map<sal_Int32, uno::Any>& deferredCharacterProperties, bool bCharContext)
+{
+    if (bCharContext)
+    {
+        assert(m_pImpl->GetTopContextType() == CONTEXT_CHARACTER);
+    }
     PropertyMapPtr rContext = m_pImpl->GetTopContext();
     for( const auto& rProp : deferredCharacterProperties )
     {

writerfilter/source/dmapper/DomainMapper_Impl.cxx
@@ -9046,12 +9046,12 @@ void DomainMapper_Impl::deferCharacterProperty(sal_Int32 id, const css::uno::Any
     m_deferredCharacterProperties[ id ] = value;
 }
 
-void DomainMapper_Impl::processDeferredCharacterProperties()
+void DomainMapper_Impl::processDeferredCharacterProperties(bool bCharContext)
 {
     // Actually process in DomainMapper, so that it's the same source file like normal processing.
     if( !m_deferredCharacterProperties.empty())
     {
-        m_rDMapper.processDeferredCharacterProperties( m_deferredCharacterProperties );
+        m_rDMapper.processDeferredCharacterProperties(m_deferredCharacterProperties, bCharContext);
         m_deferredCharacterProperties.clear();
     }
 }

MODAODF/MODA_ODF_Application_Tools

(cherry picked from commit 85f0a5d7bc54dfba75e8d6dd9c905bc1ac31d927)
Reviewed-on: https://gerrit.libreoffice.org/c/core/+/153035
Tested-by: Jenkins CollaboraOffice <jenkinscollaboraoffice@gmail.com>
Reviewed-by: Mike Kaganski <mike.kaganski@collabora.com>

writerfilter/source/dmapper/DomainMapper.cxx
@@ -1822,6 +1822,13 @@ void DomainMapper::sprmWithProps( Sprm& rSprm, const PropertyMapPtr& rContext )
             {
                 // For some undocumented reason, MS Word seems to ignore this in docDefaults
 
+                const StyleSheetEntryPtr pCurrStyle = GetStyleSheetTable()->GetCurrentEntry();
+                if (pCurrStyle && pCurrStyle->nStyleTypeCode == STYLE_TYPE_PARA && nIntValue < 0)
+                {
+                    m_pImpl->deferCharacterProperty(nSprmId, uno::Any(nIntValue));
+                    break;
+                }
+
                 // DON'T FIXME: Truly calculating this for Character Styles will be tricky,
                 // because it depends on the final fontsize - regardless of
                 // where it is set. So at the style level,
@@ -2982,9 +2989,19 @@ void DomainMapper::sprmWithProps( Sprm& rSprm, const PropertyMapPtr& rContext )
     }
 }
 
-void DomainMapper::processDeferredCharacterProperties( const std::map< sal_Int32, uno::Any >& deferredCharacterProperties )
+void DomainMapper::ProcessDeferredStyleCharacterProperties()
 {
-    assert( m_pImpl->GetTopContextType() == CONTEXT_CHARACTER );
+    assert(m_pImpl->GetTopContextType() == CONTEXT_STYLESHEET);
+    m_pImpl->processDeferredCharacterProperties(false);
+}
+
+void DomainMapper::processDeferredCharacterProperties(
+    const std::map<sal_Int32, uno::Any>& deferredCharacterProperties, bool bCharContext)
+{
+    if (bCharContext)
+    {
+        assert(m_pImpl->GetTopContextType() == CONTEXT_CHARACTER);
+    }
     PropertyMapPtr rContext = m_pImpl->GetTopContext();
     for( const auto& rProp : deferredCharacterProperties )
     {

writerfilter/source/dmapper/DomainMapper_Impl.cxx
@@ -7502,12 +7502,12 @@ void DomainMapper_Impl::deferCharacterProperty(sal_Int32 id, const css::uno::Any
     deferredCharacterProperties[ id ] = value;
 }
 
-void DomainMapper_Impl::processDeferredCharacterProperties()
+void DomainMapper_Impl::processDeferredCharacterProperties(bool bCharContext)
 {
     // Actually process in DomainMapper, so that it's the same source file like normal processing.
     if( !deferredCharacterProperties.empty())
     {
-        m_rDMapper.processDeferredCharacterProperties( deferredCharacterProperties );
+        m_rDMapper.processDeferredCharacterProperties(deferredCharacterProperties, bCharContext);
         deferredCharacterProperties.clear();
     }
 }